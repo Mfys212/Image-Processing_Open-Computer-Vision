4a4a98b4036b5d458b3f2f3106c4e3de56904636
Miftahul Huda <121081559+Mfys212@users.noreply.github.com>
2023-09-05 21:12:09 +0700

    Add files via upload

---
+++ memberi_teks.py
@@ -0,0 +1,22 @@
+import cv2
+
+filee = input("Masukkan Nama File Citra: ")
+citra = cv2.imread(filee)
+if citra is None:
+    print("Tidak dapat membaca citra", filee)
+    exit()
+else:
+    shp = citra.shape
+    if len(shp) == 2:
+        warna = 0
+    else:
+        warna = [0, 255, 0]
+tebal = 2
+y = citra.shape[1] // 2
+posisi = (10, y) #pojok kiri
+citra = cv2.putText(citra, "Computer Vision", posisi, cv2.FONT_HERSHEY_SIMPLEX, 1, warna, tebal)
+cv2.imshow("Citra dengan Garis Tengah", citra)
+cv2.waitKey()
+cv2.destroyAllWindows()
+
+
