17d750547cb5a6cde4821f3191172b2a93fbe8c7
Miftahul Huda <121081559+Mfys212@users.noreply.github.com>
2023-09-05 21:01:33 +0700

    Add files via upload

---
+++ Dasar Pengolahan Citra/membuat_bounding_box.py
@@ -0,0 +1,24 @@
+import cv2
+
+filee = input("Masukkan Nama File Citra: ")
+citra = cv2.imread(filee)
+if citra is None:
+    print("Tidak dapat membaca citra", filee)
+    exit()
+else:
+    shp = citra.shape
+    if len(shp) == 2:
+        warna = 0
+    else:
+        warna = [0, 255, 0]
+tebal = 2
+x = int(input("Koordinat x: "))
+y = int(input("Koordinat y: "))
+width = int(input("Lebar: "))
+height = int(input("Tinggi: "))
+citra = cv2.rectangle(citra, (x, y), (x+width, y+height), warna, tebal)
+cv2.imshow("Citra dengan bounding box", citra)
+cv2.waitKey()
+cv2.destroyAllWindows()
+
+
